6a2824fdb43773781a4e1d3e637d7d0a436c992b
Diego Miyabara <diego.miyabara@hotmail.com>
2020-06-09 18:57:07 -0300

    Entrega da atividade com desafios, faltando somente o riscado com onclick

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,5 +1,6 @@
 const valorInput = document.getElementById("input-tarefa")
 const diaSemana = document.getElementById("select")
+const horaSemana = document.getElementById("select-hora")
 const tarefaSegunda = document.getElementById("tarefa-segunda")
 const tarefaTerca = document.getElementById("tarefa-terca")
 const tarefaQuarta = document.getElementById("tarefa-quarta")
@@ -14,30 +15,41 @@ function enviarTarefa (){
         alert("O campo não pode estar em branco!")
     }
     else if (diaSemana.value === "segunda-feira"){
-        tarefaSegunda.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaSegunda.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "terca-feira"){
-        tarefaTerca.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaTerca.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "quarta-feira"){
-        tarefaQuarta.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaQuarta.innerHTML += `<li id="tarefa-adicionada" onclick="text-decoration = line-through">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "quinta-feira"){
-        tarefaQuinta.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaQuinta.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "sexta-feira"){
-        tarefaSexta.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaSexta.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "sabado"){
-        tarefaSabado.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaSabado.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     } else if (diaSemana.value === "domingo"){
-        tarefaDomingo.innerHTML += `<li id="tarefa-adicionada">${valorInput.value}</li>`
+        tarefaDomingo.innerHTML += `<li id="tarefa-adicionada">${horaSemana.value}: ${valorInput.value}.</li>`
         valorInput.value = ""
     }  
 }
 
-function riscar () {
-    tarefaSegunda.innerHTML = `<u> ${valorInput.value} </u>`
+// function riscar () {
+//     tarefaSegunda.innerHTML = `<u>  </u>`
+// }
+
+
+function limparAgenda (){
+    tarefaSegunda.innerHTML = ""
+    tarefaTerca.innerHTML = ""
+    tarefaQuarta.innerHTML = ""
+    tarefaQuinta.innerHTML = ""
+    tarefaSexta.innerHTML = ""
+    tarefaSabado.innerHTML = ""
+    tarefaDomingo.innerHTML = ""
 }
 
