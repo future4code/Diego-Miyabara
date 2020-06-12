cef5a386f1bd3901e1bf02620994c1016a5b32ab
Diego Miyabara <diego.miyabara@hotmail.com>
2020-06-12 10:38:55 -0300

    Exercicio de interpretação de codigo numero 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,6 @@
+//Exercícios de interpretação de código
+
+//Exercício 1 - 
+//O código executa a conversão de moeda dolar para real, o usuário digita a cotação do dolar por meio de um prompt, e o quantidade de dolar a ser convertido é inserida como 
+//parametro da função conversorDeMoeda que no exemplo foi o valor 100, esta função é inserida numa variável chamada meuDinheiro e depois impressa com um console.log
+
