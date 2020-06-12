9f2a4dd25f5aeff7f712a80db329638e921847da
Diego Miyabara <diego.miyabara@hotmail.com>
2020-06-12 10:49:09 -0300

    Exercício de interpretação numero 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -4,3 +4,10 @@
 //O código executa a conversão de moeda dolar para real, o usuário digita a cotação do dolar por meio de um prompt, e o quantidade de dolar a ser convertido é inserida como 
 //parametro da função conversorDeMoeda que no exemplo foi o valor 100, esta função é inserida numa variável chamada meuDinheiro e depois impressa com um console.log
 
+//Exercício 2 -
+//O código executa uma função chamada investeDinheiro e recebe 2 parâmetros, o tipoDeInvestimento(Poupança, Renda Fixa, CDB, Ações ou Default) e um valor que será aplicado.
+//Dentro da função foi criada uma variável chamada valorAposInvestimento para que se possa retornar o resultado do cálculo. Foi criado um switch case para calcular o valorAposInvestimento
+//com base no tipoDeInvestimento, cada um tendo seu percentual, no caso da Poupança 3%. Caso tipoDeInvestimento seja algum que não esteja listado no switch, ele retornará um alert
+//com a mensagem TIPO DE INVESTIMENTO INFORMADO INCORRETO! Caso esteja correto ele fará o cálculo e retornará o valorAposInvestimento. Fora da função, os exemplos foram Ações com 
+//o tipo e o valor 150 neste caso seria 150*1.1=165(valorAposInvestimento). O primeiro exemplo foi chamado de novoMontante e o segundo de segundoMontante. Após o cálculo deles
+//é executado um console.log com o valor calculado destes exemplos.
