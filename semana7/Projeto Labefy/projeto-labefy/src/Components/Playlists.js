71781290497a516bfb4f59b23bbd11951871b665
Diego Miyabara <diego.miyabara@hotmail.com>
2020-07-05 20:34:48 -0300

    Adição da funcionalidade de adicionar musicas e algumas estilizações

--- semana7/Projeto Labefy/projeto-labefy/src/Components/Playlists.js
+++ semana7/Projeto Labefy/projeto-labefy/src/Components/Playlists.js
@@ -1,6 +1,139 @@
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios'
+import AddTrack from './AddTrack';
+
+const PlaylistButton = styled.button`
+    width: 120px;
+    height: 30px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+    margin: 12px;
+    
+    :before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+  }
+  :active {
+    color: #000
+  }
+  :active:after {
+    background: transparent;
+  }
+  :hover:before {
+    opacity: 1;
+  }
+  :after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+  @keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+`
+const DeleteButton = styled.button`
+    color: red;
+    width: 30px;
+    height: 30px;
+    border: none;
+    outline: none;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+    margin: 12px;
+    
+    :before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+  }
+  :active {
+    color: #000
+  }
+  :active:after {
+    background: transparent;
+  }
+  :hover:before {
+    opacity: 1;
+  }
+  :after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+  @keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+`
+
+const PlaylistContainer = styled.div`
+display: flex;
+justify-content: space-between;
+width: 200px;
+margin: 10px auto;
+`
+
+const ContainerPlaylists = styled.div`
+
+`
+
+const PlaylistsBox = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+`
+
+const ContainerTracks = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+`
 
 class Playlists extends React.Component{
     state = {
@@ -71,31 +204,35 @@ class Playlists extends React.Component{
  
         return(
             <div>
-                <h2>Listas de Playlists</h2>
-                {this.state.playlists.map((playlist) => {
+                <h2>Playlists : {this.state.playlists.length}</h2>
+                <PlaylistsBox>{this.state.playlists.map((playlist) => {
                     return (
                         <div key={playlist.id}>
                             {playlist.name !== "" 
-                            ? <div>
-                                <button onClick={() => this.renderizaPlaylist(playlist.id)}>{playlist.name}</button>
+                            ? <PlaylistContainer>
+                                <PlaylistButton onClick={() => this.renderizaPlaylist(playlist.id)}>{playlist.name}</PlaylistButton>
 
-                                <button onClick={() => this.deletePlaylist(playlist.id)}>x</button>
-                            </div> 
+                                <DeleteButton onClick={() => this.deletePlaylist(playlist.id)}>x</DeleteButton>
+                            </PlaylistContainer> 
                             : <div>Não existem playlists cadastradas!</div>}
                             
                         </div>
                     )
                 })}
+                </PlaylistsBox>
                 
-                {this.state.playlistDetail && (<h3>Músicas da Playlist:</h3>)}
+                {this.state.playlistDetail && (<h3>Músicas da Playlist: {this.state.playlistDetail.length}</h3>)}
+                <ContainerTracks>
                 {this.state.playlistDetail.map((tracks) => {
                     return (
                         <div key={tracks.id}>
-                            {tracks.artist} - {tracks.name} 
-                            <div><iframe title={tracks.id} width="448" height="252" src={tracks.url} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>
+                            <strong>{tracks.artist}</strong> 
+                            <br /> {tracks.name} 
+                            <div><iframe title={tracks.id} width="224" height="176" src={tracks.url} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>
                         </div>
                     )
                 })}
+                </ContainerTracks>
             </div>
         )
     }
